c0110fbc3a03c1fed1feecff96e457fe39261526
Eugene <eugenes.mobile@gmail.com>
2015-01-10 23:54:53 -0500

    This makes it walk

--- hexarduino/hexarduino.ino
+++ hexarduino/hexarduino.ino
@@ -5,6 +5,8 @@ int ch4 = 7;
 int ch5 = 8;
 int ch6 = 9;
 
+boolean moving = false;
+
 short values[] = {1500, 1500, 1500, 1500, 1500, 1500};
 
 void setup() {
@@ -15,23 +17,65 @@ void setup() {
   pinMode(ch5, INPUT);
   pinMode(ch6, INPUT);
   Serial.begin(115200);
+  
+  Serial.println("LH1000 LM1400 LL1800 RH1800 RM1400 RL1000 VS3000");
+  delay(50);
+  Serial.println("LF1700 LR1300 RF1300 RR1700 HT1500");
+  delay(50);
+  Serial.println("#8P1500#9P1500#10P1500#24P1500#25P1500#26P1500T2000");
 }
 
 void loop() {
+  //Get readings from RC-receiver channels
+  values[0] = pulseIn(ch1, HIGH)-1300;
+  values[1] = pulseIn(ch2, HIGH)-1300;
+  values[2] = pulseIn(ch3, HIGH)-1300;
+  values[3] = pulseIn(ch4, HIGH)-1300;
+  values[4] = pulseIn(ch5, HIGH)-1300;
+  values[5] = pulseIn(ch6, HIGH)-1300;
   
-  values[0] = pulseIn(ch1, HIGH);
-  values[1] = pulseIn(ch2, HIGH);
-  values[2] = pulseIn(ch3, HIGH);
-  values[3] = pulseIn(ch4, HIGH);
-  values[4] = pulseIn(ch5, HIGH);
-  values[5] = pulseIn(ch6, HIGH);
+  //Moving speed is read from channel 1 (right stick, top-bottom direction)
+  int movingSpeed = int(values[1] / 3.20);  //Normalize the value
+  if (movingSpeed <10 && movingSpeed > -10) movingSpeed = 0;  //And remove noise
+
+  //Turning speed is read from channel 0 (right stick, left-right direction)
+  int turningSpeed = int(values[0] / 3.20);  //Normalize
+  if (turningSpeed <10 && turningSpeed > -10) turningSpeed = 0;  //Remove noise
   
-  for (int idx = 0; idx<6; idx++) {
-    Serial.print("#");
-    Serial.print(idx);
-    Serial.print("P");
-    Serial.print(values[idx], DEC);
+  //Lynxmotion SSC-32 already has some gaits pre-configured, and 
+  // gives you API that looks like commanding a tank (by applying 
+  // speed to motors connected to it's left and right tracks).
+  int xl = movingSpeed;  //Left track speed
+  int xr = movingSpeed;  //Right track speed
+  //If you want your tank to move forward, you give both tracks 
+  //the same speed. If you want to turn, slow down track on the inner 
+  //side of the turn.
+  if (movingSpeed < 10 && movingSpeed > -10 && turningSpeed < 10 && turningSpeed > -10) {
+    if (moving) {
+      Serial.println("XSTOP");
+    }
+    moving = false;
+  } else {
+    moving = true;
+    if (movingSpeed<0) {
+      xr = xr + turningSpeed;
+      xl = xl - turningSpeed;
+    } else {
+      xr = xr - turningSpeed;
+      xl = xl + turningSpeed;
+    }
+  
+    if (xl < 0) xl = max(xl, -100);
+    if (xl > 0) xl = min(xl, 100);
+    if (xr < 0) xr = max(xr, -100);
+    if (xr > 0) xr = min(xr, 100);
+
+    Serial.print("XL");
+    Serial.print(xl, DEC); 
+    Serial.print(" XR");
+    Serial.print(xr, DEC); 
+    Serial.println(" XS100");
   }
-  Serial.println("T20");
-  delay(30);
+    
+  delay(100);
 }
--- readme.md
+++ readme.md
@@ -1,1 +1,33 @@
 Hexapod build log
+
+Right stick: 
+
+Middle: 
+ch0 ~400
+ch1 ~390
+
+Top: 
+ch0 ~400
+ch1 ~715
+
+Bottom: 
+ch0 ~
+ch1 ~
+
+Left: 
+Bottom: 
+ch0 ~
+ch1 ~
+
+Right: 
+Bottom: 
+ch0 ~
+ch1 ~
+
+Top right: 
+ch0 ~
+ch1 ~
+
+Bottom left: 
+ch0 ~
+ch1 ~
