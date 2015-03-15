6a57c66dfd5ce7cae7b7dd40a71f4c0a9ee48ae5
Eugene <eugenes.mobile@gmail.com>
2015-03-14 23:11:51 -0400

    Changes in gait setup

--- hexarduino/hexarduino.ino
+++ hexarduino/hexarduino.ino
@@ -5,7 +5,7 @@ int ch4 = 6;
 int ch5 = 5;
 int ch6 = 4;
 int led = 13;
-
+int mediumLegPosition = 0;
 boolean moving = false;
 
 short values[] = {1500, 1500, 1500, 1500, 1500, 1500};
@@ -23,28 +23,29 @@ void setup() {
   
   returnLegsToInitialPosition();
 
-  Serial.println("LH1000 LM1200 LL1800 RH1800 RM1200 RL1000 VS3000");
-  delay(50);
-  Serial.println("LF1700 LR1300 RF1300 RR1700 HT300");
-  delay(50);
+  setupGaitValues();
 }
 
 void loop() {
   //Get readings from RC-receiver channels
-  values[0] = pulseIn(ch1, HIGH)-1300;
-  values[1] = pulseIn(ch2, HIGH)-1300;
-  values[2] = pulseIn(ch3, HIGH)-1300;
-  values[3] = pulseIn(ch4, HIGH)-1300;
-  values[4] = pulseIn(ch5, HIGH)-1300;
-  values[5] = pulseIn(ch6, HIGH)-1300;
+  values[0] = pulseIn(ch1, HIGH)-1500;
+  values[1] = pulseIn(ch2, HIGH)-1500;
+  values[2] = pulseIn(ch3, HIGH)-1500;
+  values[3] = pulseIn(ch4, HIGH)-1500;
+  values[4] = pulseIn(ch5, HIGH)-1500;
+  //values[5] = pulseIn(ch6, HIGH)-1300;
   
   //Moving speed is read from channel 1 (right stick, top-bottom direction)
   int movingSpeed = int(values[1] / 3.20);  //Normalize the value
+  //Serial.print("--movingSpeed=");
+  //Serial.println(movingSpeed,DEC);
   if (movingSpeed <10 && movingSpeed > -10) movingSpeed = 0;  //And remove noise
 
   //Turning speed is read from channel 0 (right stick, left-right direction)
   int turningSpeed = int(values[0] / 3.20);  //Normalize
   if (turningSpeed <10 && turningSpeed > -10) turningSpeed = 0;  //Remove noise
+  //Serial.print("--turningSpeed=");
+  //Serial.println(turningSpeed,DEC);
   
   //Lynxmotion SSC-32 already has some gaits pre-configured, and 
   // gives you API that looks like commanding a tank (by applying 
@@ -59,6 +60,9 @@ void loop() {
       Serial.println("XSTOP");
       delay(30);
       returnLegsToInitialPosition();
+    } else {
+      setupGaitValues();
+      delay(30);
     }
     moving = false;
   } else {
@@ -78,14 +82,46 @@ void loop() {
 
     digitalWrite(led, HIGH);
     Serial.print("XL");
-    Serial.print(xl, DEC); 
+    Serial.print(xl, DEC);
     Serial.print(" XR");
     Serial.print(xr, DEC); 
     Serial.println(" XS200");
   }
     
-  delay(100);
+  delay(250);
   digitalWrite(led, LOW);
+}
+
+void setupGaitValues() {
+  int newMediumPosition = 0;
+  while (newMediumPosition<900 || newMediumPosition>1900) {
+    newMediumPosition = pulseIn(ch6, HIGH);
+  }
+  
+  if (abs(newMediumPosition-mediumLegPosition)<50) return;
+  mediumLegPosition = newMediumPosition;
+  int low = mediumLegPosition-400;
+  int high = mediumLegPosition+400;
+  String gaitSetup = "LH";
+  gaitSetup += low;
+  gaitSetup += " LM";
+  gaitSetup += mediumLegPosition;
+  gaitSetup += " LL";
+  gaitSetup += high;
+  gaitSetup += " RH";
+  gaitSetup += high;
+  gaitSetup += " RM";
+  gaitSetup += mediumLegPosition;
+  gaitSetup += " RL";
+  gaitSetup += low;
+  
+  Serial.println("LH1000 LM1400 LL1800 RH1800 RM1400 RL1000 VS3000");
+
+  gaitSetup += " VS3000";
+  Serial.println(gaitSetup);
+  delay(50);
+  Serial.println("LF1700 LR1300 RF1300 RR1700 HT300");
+  delay(50);
 }
 
 void returnLegsToInitialPosition() {
@@ -98,9 +134,9 @@ void returnLegsToInitialPosition() {
   delay(30);
   Serial.print("#17P1500#19P1500#21P1500");
   delay(30);
-  Serial.println("#0P1300#2P1300#4P1300");
+  Serial.print("#0P1300#2P1300#4P1300");
   delay(30);
   Serial.println("#16P1700#18P1700#20P1700T500");
-  delay(2000);
+  delay(600);
   digitalWrite(led, LOW);
 }
